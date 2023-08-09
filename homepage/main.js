e749bbc0a0ee1684a8e40dc41f3aa3ea6c8a7536
ozmvege <oezenucan.99@gmail.com>
2023-08-09 07:03:55 -0700

    logo

--- homepage/main.js
+++ homepage/main.js
@@ -61,11 +61,3 @@ function calculate() {
 	}
 }
 
-const cursor = document.querySelector('.custom-cursor');
-
-document.addEventListener('mousemove', (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
-  cursor.style.left = x + 'px';
-  cursor.style.top = y + 'px';
-});
